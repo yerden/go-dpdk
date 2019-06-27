eacf0d20dd090def89378d78532b86c45df0f796
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2019-06-27 14:35:50 +0600

    common: add object register
    Add capability to register/unregister objects and address them by id.
    Useful to pass Go data through Cgo call by reference.
    
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

---
+++ common/objects.go
@@ -0,0 +1,104 @@
+package common
+
+import (
+	"sync"
+)
+
+// global map of objects
+var (
+	registry = &objTable{
+		hash: make(map[ObjectID]interface{}),
+	}
+)
+
+type ObjectID uint64
+
+// Registry implements CRUD operations to map ID and objects.
+type Registry interface {
+	Create(interface{}) ObjectID
+	Read(ObjectID) interface{}
+	Update(ObjectID, interface{})
+	Delete(ObjectID)
+}
+
+func NewRegistryMap() Registry {
+	return &objTable{
+		hash: make(map[ObjectID]interface{}),
+	}
+}
+
+// map of objects references with lock
+type objTable struct {
+	sync.Mutex
+	hash map[ObjectID]interface{}
+	id   ObjectID
+}
+
+func (r *objTable) Create(obj interface{}) ObjectID {
+	r.Lock()
+	id := r.id
+	r.hash[id] = obj
+	r.id = id + 1
+	r.Unlock()
+	return id
+}
+
+func (r *objTable) Read(id ObjectID) interface{} {
+	r.Lock()
+	obj := r.hash[id]
+	r.Unlock()
+	return obj
+}
+
+func (r *objTable) Update(id ObjectID, obj interface{}) {
+	r.Lock()
+	r.hash[id] = obj
+	r.Unlock()
+}
+
+func (r *objTable) Delete(id ObjectID) {
+	r.Lock()
+	delete(r.hash, id)
+	r.Unlock()
+}
+
+type objArray struct {
+	sync.Mutex
+	array []interface{}
+	cnt   uint64
+}
+
+func (r *objArray) Create(obj interface{}) ObjectID {
+	r.Lock()
+	id := ObjectID(len(r.array))
+	r.array = append(r.array, obj)
+	r.cnt += 1
+	r.Unlock()
+	return id
+}
+
+func (r *objArray) Read(id ObjectID) interface{} {
+	r.Lock()
+	obj := r.array[id]
+	r.Unlock()
+	return obj
+}
+
+func (r *objArray) Update(id ObjectID, obj interface{}) {
+	r.Lock()
+	r.array[id] = obj
+	r.Unlock()
+}
+
+func (r *objArray) Delete(id ObjectID) {
+	r.Lock()
+	r.array[id] = nil
+	if r.cnt -= 1; r.cnt == 0 {
+		r.array = make([]interface{}, 0, 10)
+	}
+	r.Unlock()
+}
+
+func NewRegistryArray() Registry {
+	return &objArray{}
+}
