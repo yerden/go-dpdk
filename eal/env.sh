37b4048444093fb0c0c7a9e515677d97d557a201
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2019-10-27 00:41:43 +0600

    eal: remove env.sh
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- eal/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# set path to dpdk installation
-export RTE_SDK=$1
-
-# example to Linux@x86_x64
-export RTE_TARGET=x86_64-native-linux-gcc
-export CGO_CFLAGS="-m64 -pthread -O3 -march=native -I$RTE_SDK/$RTE_TARGET/include"
-export CGO_LDFLAGS="-L$RTE_SDK/$RTE_TARGET/lib -ldpdk -lz -lrt -lnuma -ldl -lm"
