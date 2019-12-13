d9a675ead2e2489ebcbd133a3db4c8f819d23bc2
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2019-12-14 01:04:00 +0600

    app/helloworld: fix to use new EAL API
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- app/helloworld/main.go
+++ app/helloworld/main.go
@@ -1,42 +1,21 @@
 package main
 
 import (
-	"golang.org/x/sys/unix"
 	"log"
 	"os"
-	"sync"
 
 	"github.com/yerden/go-dpdk/eal"
 )
 
 func main() {
-	var set unix.CPUSet
-
-	if err := unix.SchedGetaffinity(0, &set); err != nil {
-		panic(err)
+	if _, err := eal.Init(os.Args); err != nil {
+		log.Fatalln("EAL init failed:", err)
 	}
+	defer eal.Cleanup()
 
-	err := eal.InitWithParams(os.Args[0],
-		eal.NewParameter("-c", eal.NewMap(&set)),
-		eal.NewParameter("--no-huge"),
-		eal.NewParameter("--no-pci"),
-		eal.NewParameter("--master-lcore", 0),
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	var wg sync.WaitGroup
-	for _, id := range eal.Lcores(false) {
-		wg.Add(1)
-		eal.ExecuteOnLcore(id, func(lcore *eal.Lcore) {
-			defer wg.Done()
-			log.Println("Executed on lcore", lcore.ID())
+	for _, id := range eal.Lcores() {
+		eal.ExecOnLcore(id, func(ctx *eal.LcoreCtx) {
+			log.Println("hello from core", ctx.LcoreID())
 		})
-	}
-	wg.Wait()
-
-	if err := eal.Cleanup(); err != nil {
-		panic(err)
 	}
 }
