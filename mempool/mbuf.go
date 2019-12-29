8112e8398ae4d78882e87911117a4f9218251998
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2019-12-29 13:36:14 +0600

    mempool: use rte_pktmbuf_pool_create_by_ops in CreateMbufPool
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- mempool/mbuf.go
+++ mempool/mbuf.go
@@ -13,7 +13,6 @@ static int is_priv_size_aligned(uint16_t priv_size) {
 import "C"
 
 import (
-	"syscall"
 	"unsafe"
 )
 
@@ -36,48 +35,20 @@ func CreateMbufPool(name string, n uint32, dataRoomSize uint16, opts ...Option) 
 		opts[i].f(conf)
 	}
 
-	// check alignment
-	if C.is_priv_size_aligned(C.uint16_t(conf.privDataSize)) == 0 {
-		return nil, syscall.EINVAL
-	}
-
-	// calculate element size
-	eltSize := C.uint(dataRoomSize) + C.sizeof_struct_rte_mbuf + conf.privDataSize
 	cname := C.CString(name)
 	defer C.free(unsafe.Pointer(cname))
-
-	mbpPriv := &C.struct_rte_pktmbuf_pool_private{
-		mbuf_data_room_size: C.uint16_t(dataRoomSize),
-		mbuf_priv_size:      C.uint16_t(conf.privDataSize),
+	var cOps *C.char
+	if conf.opsName != nil {
+		cOps = C.CString(*conf.opsName)
+		defer C.free(unsafe.Pointer(cOps))
 	}
+	mp := (*Mempool)(C.rte_pktmbuf_pool_create_by_ops(cname, C.uint(n),
+		conf.cacheSize, C.ushort(conf.privDataSize), C.uint16_t(dataRoomSize),
+		conf.socket, cOps))
 
-	// create mempool
-	mp := (*Mempool)(C.rte_mempool_create_empty(cname, C.uint(n), eltSize,
-		conf.cacheSize, C.sizeof_struct_rte_pktmbuf_pool_private, conf.socket, 0))
 	if mp == nil {
 		return nil, err()
 	}
 
-	if conf.opsName == nil {
-		defaultOps := "ring_mp_mc"
-		conf.opsName = &defaultOps
-	}
-
-	// set ops
-	if err := mp.SetOpsByName(*conf.opsName, nil); err != nil {
-		mp.Free()
-		return nil, err
-	}
-
-	// init and populate pool
-	C.rte_pktmbuf_pool_init((*C.struct_rte_mempool)(mp), unsafe.Pointer(mbpPriv))
-
-	if _, err := mp.PopulateDefault(); err != nil {
-		mp.Free()
-		return nil, err
-	}
-
-	// initialize objects
-	mp.objIterC(C.rte_pktmbuf_init, nil)
 	return mp, nil
 }
