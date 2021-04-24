a82a7198d28e1dcc4feb07e479635aff3525fd0e
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2021-04-24 18:02:24 +0600

    mempool: use new EAL API
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- mempool/mempool_test.go
+++ mempool/mempool_test.go
@@ -20,7 +20,7 @@ var initEAL = common.DoOnce(func() error {
 			"-m", "128",
 			"--no-huge",
 			"--no-pci",
-			"--master-lcore", "0"})
+			"--main-lcore", "0"})
 	}
 	return err
 })
@@ -31,8 +31,8 @@ func TestMempoolCreateErr(t *testing.T) {
 	// Initialize EAL on all cores
 	assert(initEAL() == nil)
 
-	// create and test mempool on master lcore
-	err := eal.ExecOnMaster(func(ctx *eal.LcoreCtx) {
+	// create and test mempool on main lcore
+	err := eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
 		// create empty mempool
 		n := uint32(10240)
 		mp, err := mempool.CreateEmpty("test_mp",
@@ -54,8 +54,8 @@ func TestMempoolPriv(t *testing.T) {
 	// Initialize EAL on all cores
 	assert(initEAL() == nil)
 
-	// create and test mempool on master lcore
-	err := eal.ExecOnMaster(func(ctx *eal.LcoreCtx) {
+	// create and test mempool on main lcore
+	err := eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
 		// create empty mempool
 		n := uint32(10240)
 		mp, err := mempool.CreateEmpty("test_mp",
@@ -76,13 +76,13 @@ func TestMempoolPriv(t *testing.T) {
 }
 
 func TestMempoolCreate(t *testing.T) {
-	assert := common.Assert(t, true)
+	assert := common.Assert(t, false)
 
 	// Initialize EAL on all cores
 	assert(initEAL() == nil)
 
-	// create and test mempool on master lcore
-	err := eal.ExecOnMaster(func(ctx *eal.LcoreCtx) {
+	// create and test mempool on main lcore
+	err := eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
 		// create empty mempool
 		n := uint32(10240)
 		mp, err := mempool.CreateEmpty("test_mp",
@@ -90,7 +90,7 @@ func TestMempoolCreate(t *testing.T) {
 			2048, // size of element
 			mempool.OptSocket(int(ctx.SocketID())),
 			mempool.OptCacheSize(32),
-			mempool.OptOpsName("stack"),
+			//mempool.OptOpsName("stack"),
 			mempool.OptPrivateDataSize(1024),
 		)
 		assert(err == nil, err)
@@ -127,8 +127,8 @@ func TestMempoolCreate(t *testing.T) {
 	})
 	assert(err == nil, err)
 
-	// create and test mempool on master lcore
-	err = eal.ExecOnMaster(func(ctx *eal.LcoreCtx) {
+	// create and test mempool on main lcore
+	err = eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
 		// create empty mempool
 		n := uint32(10240)
 		mp, err := mempool.CreateMbufPool("test_mbuf_pool",
