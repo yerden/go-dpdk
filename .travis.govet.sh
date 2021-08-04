d9e013ffd6ccc2e8f5f65400dbe96034aef7ea1f
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2021-08-04 14:54:48 +0600

    ci: move to v21.05
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- .travis.govet.sh
+++ .travis.govet.sh
@@ -2,8 +2,6 @@
 
 cd "$(dirname $0)"
 
-export CGO_LDFLAGS="-Wl,--dynamic-linker=/lib64/ld-linux-x86-64.so.2"
-
 set -e
 DIRS="common eal ethdev lcore mempool ring port memzone ethdev/flow"
 # Add subdirectories here as we clean up golint on each.
