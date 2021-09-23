dff920e743d79255ec3b85b5f1c8784dcabee9c0
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2021-09-23 15:59:48 +0600

    util: add error wrapper
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

---
+++ util/errors.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// ErrWithMessage wraps Err and buffer to store annotation.
+type ErrWithMessage struct {
+	Err error
+	B   bytes.Buffer
+}
+
+func (e *ErrWithMessage) Error() string {
+	return fmt.Sprintf("%v: %v", string(e.B.Bytes()), e.Err)
+}
+
+func (e *ErrWithMessage) Unwrap() error {
+	return e.Err
+}
+
+// ErrWrapf annotates specified err with formatted string.
+func ErrWrapf(err error, format string, args ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+	e := &ErrWithMessage{Err: err}
+	fmt.Fprintf(&e.B, format, args...)
+	return e
+}
+
+// ErrWrap annotates specified err with message string.
+func ErrWrap(err error, msg string) error {
+	return ErrWrapf(err, msg)
+}
