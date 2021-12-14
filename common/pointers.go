943a775ab071549950453a8be73a2c63f681716c
Ainura <a.kuraliyeva@kazdream.kz>
2021-12-14 15:48:40 +0600

    common/pointers: add CArray struct
    Signed-off-by: Ainura <a.kuraliyeva@kazdream.kz>

--- common/pointers.go
+++ common/pointers.go
@@ -14,3 +14,11 @@ func MakeSlice(buf unsafe.Pointer, max int) []byte {
 	sh.Cap = max
 	return dst
 }
+
+// CArray is a GO structure representation of a C array.
+// Ptr is a pointer to the beginning of the C array and Len is its maximum length.
+// CArray has a certain length, don't try to extend it.
+type CArray struct {
+	Ptr unsafe.Pointer
+	Len int
+}
