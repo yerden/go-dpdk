cd5bc5635c966601070d1012e9a9ce3ac8333f92
Ainura <a.kuraliyeva@kazdream.kz>
2021-12-14 15:57:35 +0600

    mempool/mempool_test: remove duplicate code;
    add test and benchmark
    
    Signed-off-by: Ainura <a.kuraliyeva@kazdream.kz>

--- mempool/mempool_test.go
+++ mempool/mempool_test.go
@@ -28,6 +28,38 @@ var initEAL = common.DoOnce(func() error {
 	return err
 })
 
+func assert(t testing.TB, expected bool, args ...interface{}) {
+	if !expected {
+		t.Helper()
+		t.Fatal(args...)
+	}
+}
+
+func doOnMain(t *testing.T, fn func(p *mempool.Mempool, data []byte)) {
+	// Initialize EAL on all cores
+	assert(t, initEAL() == nil)
+
+	data := []byte("hello from Mbuf")
+	// create and test mempool on main lcore
+	err := eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
+		// create empty mempool
+		n := uint32(10240)
+		mp, err := mempool.CreateMbufPool("test_mbuf_pool",
+			n,    // elements count
+			2048, // size of element
+			mempool.OptSocket(int(eal.SocketID())),
+			mempool.OptCacheSize(32),
+			mempool.OptOpsName("stack"),
+			mempool.OptPrivateDataSize(64), // for each Mbuf
+		)
+		assert(t, err == nil, err)
+		assert(t, mp != nil)
+		defer mp.Free()
+		fn(mp, data)
+	})
+	assert(t, err == nil)
+}
+
 func TestMempoolCreateErr(t *testing.T) {
 	assert := common.Assert(t, true)
 
@@ -47,33 +79,6 @@ func TestMempoolCreateErr(t *testing.T) {
 			mempool.OptPrivateDataSize(1024),
 		)
 		assert(mp == nil && err == syscall.EINVAL, err)
-	})
-	assert(err == nil, err)
-}
-
-func TestMempoolPriv(t *testing.T) {
-	assert := common.Assert(t, true)
-
-	// Initialize EAL on all cores
-	assert(initEAL() == nil)
-
-	// create and test mempool on main lcore
-	err := eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
-		// create empty mempool
-		n := uint32(10240)
-		mp, err := mempool.CreateEmpty("test_mp",
-			n,    // elements count
-			2048, // size of element
-			mempool.OptSocket(int(eal.SocketID())),
-			mempool.OptCacheSize(32), // too large
-			mempool.OptOpsName("stack"),
-			mempool.OptPrivateDataSize(1024),
-		)
-		assert(mp != nil && err == nil, err)
-		defer mp.Free()
-
-		priv := mp.GetPrivBytes()
-		assert(len(priv) == 1024, len(priv))
 	})
 	assert(err == nil, err)
 }
@@ -130,54 +135,10 @@ func TestMempoolCreate(t *testing.T) {
 	})
 	assert(err == nil, err)
 
-	// create and test mempool on main lcore
-	err = eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
-		// create empty mempool
-		n := uint32(10240)
-		mp, err := mempool.CreateMbufPool("test_mbuf_pool",
-			n,    // elements count
-			2048, // size of element
-			mempool.OptSocket(int(eal.SocketID())),
-			mempool.OptCacheSize(32),
-			mempool.OptOpsName("stack"),
-			mempool.OptPrivateDataSize(64), // for each mbuf
-		)
-		assert(err == nil, err)
-		assert(mp != nil)
-		defer mp.Free()
-
-		data := []byte("hello from mbuf")
-		myMbuf := mbuf.PktMbufAlloc(mp)
-		mbuf.PktMbufAppend(myMbuf, data)
-		assert(bytes.Equal(myMbuf.Data(), data))
-		memp := myMbuf.GetPool()
-		mymp, err := mempool.Lookup("test_mbuf_pool")
-		assert(mymp == memp)
-		assert(err == nil)
-		defer mbuf.PktMbufFree(myMbuf)
-
-		var mbufArr []*mbuf.Mbuf
-		mbufArr = make([]*mbuf.Mbuf, 4)
-		err = mbuf.PktMbufAllocBulk(mp, mbufArr)
-		assert(err == nil)
-		for _, m := range mbufArr {
-			mbuf.PktMbufAppend(m, data)
-			assert(bytes.Equal(m.Data(), data))
-		}
-
-		for _, m := range mbufArr {
-			mbuf.PktMbufReset(m)
-			assert(bytes.Equal(m.Data(), []byte{}))
-		}
-
-		var mbufArrEmpty []*mbuf.Mbuf
-		err = mbuf.PktMbufAllocBulk(mp, mbufArrEmpty)
-		assert(err == nil)
-		assert(len(mbufArrEmpty) == 0)
-
-		mp, err = mempool.CreateMbufPool("test_mbuf_pool_err",
-			n,    // elements count
-			2048, // size of element
+	doOnMain(t, func(p *mempool.Mempool, data []byte) {
+		p, err = mempool.CreateMbufPool("test_mbuf_pool_err",
+			10240, // elements count
+			2048,  // size of element
 			mempool.OptSocket(int(eal.SocketID())),
 			mempool.OptCacheSize(32),
 			mempool.OptOpsName("stack"),
@@ -185,20 +146,85 @@ func TestMempoolCreate(t *testing.T) {
 		)
 		assert(err != nil, err)
 	})
-	assert(err == nil, err)
+}
+
+func TestMempoolPriv(t *testing.T) {
+	doOnMain(t, func(p *mempool.Mempool, data []byte) {
+		priv := p.GetPrivBytes()
+		assert(t, len(priv) == 64, len(priv))
+	})
+}
+
+// TODO: add docs
+func TestMbufMethods(t *testing.T) {
+	doOnMain(t, func(p *mempool.Mempool, data []byte) {
+		myMbuf := mbuf.PktMbufAlloc(p)
+		mbuf.PktMbufReset(myMbuf)
+		mbuf.PktMbufAppend(myMbuf, data)
+		assert(t, bytes.Equal(myMbuf.Data(), data))
+		memp := myMbuf.GetPool()
+		mymp, err := mempool.Lookup("test_mbuf_pool")
+		assert(t, mymp == memp)
+		assert(t, err == nil)
+		defer mbuf.PktMbufFree(myMbuf)
+
+		var mbufArr []*mbuf.Mbuf
+		mbufArr = make([]*mbuf.Mbuf, 4)
+		err = mbuf.PktMbufAllocBulk(p, mbufArr)
+		assert(t, err == nil)
+		for _, m := range mbufArr {
+			mbuf.PktMbufAppend(m, data)
+			assert(t, bytes.Equal(m.Data(), data))
+		}
+
+		for _, m := range mbufArr {
+			mbuf.PktMbufReset(m)
+			assert(t, bytes.Equal(m.Data(), []byte{}))
+		}
+
+		var mbufArrEmpty []*mbuf.Mbuf
+		err = mbuf.PktMbufAllocBulk(p, mbufArrEmpty)
+		assert(t, err == nil)
+		assert(t, len(mbufArrEmpty) == 0)
+	})
 }
 
 func TestMbufpoolPriv(t *testing.T) {
-	assert := common.Assert(t, false)
+	doOnMain(t, func(p *mempool.Mempool, data []byte) {
+		myMbuf := mbuf.PktMbufAlloc(p)
+		mData := myMbuf.GetPrivData()
+		assert(t, mData.Len == int(myMbuf.GetPrivSize()))
+		ptrSlice := common.MakeSlice(mData.Ptr, mData.Len)
 
-	// Initialize EAL on all cores
-	assert(initEAL() == nil)
+		copy(ptrSlice, data)
+		newData := myMbuf.GetPrivData()
+		newSlice := common.MakeSlice(newData.Ptr, newData.Len)
+		assert(t, bytes.Equal(data, newSlice[:len(data)]))
+		assert(t, newData.Len == int(myMbuf.GetPrivSize()))
+		mbuf.PktMbufFree(myMbuf)
+	})
+}
 
-	// create and test mempool on main lcore
-	err := eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
-		// create empty mempool
-		n := uint32(10240)
-		mp, err := mempool.CreateMbufPool("test_mbuf_pool_priv",
+func TestAllocResetAppend(t *testing.T) {
+	doOnMain(t, func(p *mempool.Mempool, data []byte) {
+		myMbuf := mbuf.PktMbufAlloc(p)
+		myMbuf.ResetAndAppend(data)
+		assert(t, bytes.Equal(myMbuf.Data(), data))
+
+		newMbuf := mbuf.AllocResetAndAppend(p, data)
+		assert(t, newMbuf != nil)
+		assert(t, bytes.Equal(newMbuf.Data(), data))
+	})
+
+}
+
+func BenchmarkAllocFromChannel(b *testing.B) {
+	initEAL()
+	n := uint32(10240)
+	ch := make(chan *mbuf.Mbuf, n)
+	data := []byte("Some data for test")
+	eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
+		mp, _ := mempool.CreateMbufPool("test_mbuf_pool",
 			n,    // elements count
 			2048, // size of element
 			mempool.OptSocket(int(eal.SocketID())),
@@ -206,22 +232,40 @@ func TestMbufpoolPriv(t *testing.T) {
 			mempool.OptOpsName("stack"),
 			mempool.OptPrivateDataSize(64), // for each mbuf
 		)
-		assert(err == nil, err)
-		assert(mp != nil)
 		defer mp.Free()
-
-		data := []byte("hello from private area")
-		myMbuf := mbuf.PktMbufAlloc(mp)
-		mData := myMbuf.GetPrivData()
-		assert(mData.Len == int(myMbuf.GetPrivSize()))
-		ptrSlice := common.MakeSlice(mData.Ptr, mData.Len)
-
-		copy(ptrSlice, data)
-		newData := myMbuf.GetPrivData()
-		newSlice := common.MakeSlice(newData.Ptr, newData.Len)
-		assert(bytes.Equal(data, newSlice[:len(data)]))
-		assert(newData.Len == int(myMbuf.GetPrivSize()))
-		mbuf.PktMbufFree(myMbuf)
+		mbufArr := make([]*mbuf.Mbuf, n)
+		mbuf.PktMbufAllocBulk(mp, mbufArr)
+		for _, m := range mbufArr {
+			ch <- m
+		}
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			myMbuf := <-ch
+			myMbuf.ResetAndAppend(data)
+			ch <- myMbuf
+		}
 	})
-	assert(err == nil, err)
+	close(ch)
+}
+
+func BenchmarkAllocFromMempool(b *testing.B) {
+	initEAL()
+	n := uint32(10240)
+	data := []byte("Some data for test")
+	eal.ExecOnMain(func(ctx *eal.LcoreCtx) {
+		mp, _ := mempool.CreateMbufPool("test_mbuf_pool",
+			n,    // elements count
+			2048, // size of element
+			mempool.OptSocket(int(eal.SocketID())),
+			mempool.OptCacheSize(32),
+			mempool.OptOpsName("stack"),
+			mempool.OptPrivateDataSize(64), // for each mbuf
+		)
+		defer mp.Free()
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			newMbuf := mbuf.AllocResetAndAppend(mp, data)
+			mbuf.PktMbufFree(newMbuf)
+		}
+	})
 }
