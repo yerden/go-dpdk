0d5e6ff539e6f978ba394ca08ed214978052bdad
Ainura <a.kuraliyeva@kazdream.kz>
2022-01-21 11:02:30 +0600

    common/pointers: add CStruct and methods
    Signed-off-by: Ainura <a.kuraliyeva@kazdream.kz>

--- common/pointers.go
+++ common/pointers.go
@@ -14,3 +14,30 @@ func MakeSlice(buf unsafe.Pointer, max int) []byte {
 	sh.Cap = max
 	return dst
 }
+
+// CStruct is a GO structure representation of a C array.
+// CStruct has a certain length, don't try to extend it.
+type CStruct struct {
+	// Ptr is a pointer to the beginning of the C array.
+	Ptr unsafe.Pointer
+	// Len is the size of memory area pointed by Ptr.
+	Len int
+}
+
+// Init initializes CStruct instance with specified pointer ptr and
+// length len.
+func (cs *CStruct) Init(ptr unsafe.Pointer, len int) {
+	cs.Ptr = ptr
+	cs.Len = len
+}
+
+// Bytes converts C array into slice of bytes backed by this array.
+// Returned slice cannot be extended or it will be reallocated into Go
+// memory.
+func (cs *CStruct) Bytes() (dst []byte) {
+	sh := (*reflect.SliceHeader)(unsafe.Pointer(&dst))
+	sh.Data = uintptr(cs.Ptr)
+	sh.Len = cs.Len
+	sh.Cap = cs.Len
+	return
+}
